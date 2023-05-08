249d9ecf6209ebbdb5ef9df5adfc77e7ddd0aa1f
shelbyhust <shelby.e.hust@gmail.com>
2023-05-08 14:28:53 -0600

    Revise summary

--- query.sql
+++ query.sql
@@ -42,18 +42,17 @@ WHERE LOWER(de.mail) NOT LIKE '%cu.edu'
     )
     AND daf.description NOT LIKE 'POI_%'; --need to escape '_' if 'POI_' should be matched literally
 
-/*This SQL script selects a username and email and creates a new column 
-to classify people in a database under 'person_type' based on their primary 
-affiliation, taken from the dir_person table, and their education affiliation
-(edupersonaffiliation), taken from the dir_affiliation table. The person_type 
-column is categorized into 'Student' or 'Faculty/Staff.'
+/*This SQL script selects a username, email 'person_type' from three tables. 
+The 'person_type' column is derived using 'CASE' statements based on a user's
+primary affiliation, taken from the dir_person table, and their education 
+affiliation (edupersonaffiliation), taken from the dir_affiliation table. 
+The person_type column is categorized into 'Student' or 'Faculty/Staff.'
 Certain conditions must be met in order for a user to be selected. For example, 
-former students, retired faculty/staff, or spouses should not be selected. An 
+former students, retired faculty/staff, or spouses should not be selected. If 
+a user is a student, their id should be found in the dir_acad_career table. An 
 inner join is performed on the dir_person and edupersonaffiliation tables. In 
 addition, email addresses from dir_email are also selected, with a left join 
-from the two previous tables to the dir_email table. If a user is not classified 
-as a student, their email address should not end in cu.edu. If a user is a 
-student, their id should be found in the dir_acad_career table.
+from the two previous tables to the dir_email table. 
 
 Summary: This SQL script retrieves a distinct list of usernames, emails, and 
 their associated person type for those affiliated with the CU Boulder Campus,
