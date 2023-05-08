ac1469e352202ddb3c381324c2e7b7c261c8a995
shelbyhust <shelby.e.hust@gmail.com>
2023-05-07 19:08:21 -0600

    Adds summary of script

--- query.sql
+++ query.sql
@@ -1,3 +1,5 @@
+--Original Script
+
 select distinct dp.dir_uid as username
   ,de.mail as email
   ,case  
@@ -61,3 +63,21 @@ where (
   )
   and de.mail is not NULL
   and lower(de.mail) not like '%cu.edu'
+
+/*This SQL script selects a username and email and creates a new column 
+to classify people in a database under 'person_type' based on their primary 
+affiliation, taken from the dir_person table, and their education affiliation
+(edupersonaffiliation), taken from the dir_affiliation table. The person_type 
+column is categorized into 'Student', 'Faculty/Staff', or 'Student/Faculty.' 
+Certain conditions must be met in order for a user to be selected. For example, 
+former students, retired faculty/staff, or spouses should not be selected. An 
+inner join is performed on the dir_person and edupersonaffiliation tables. In 
+addition, email addresses from dir_email are also selected, with a left join 
+from the two previous tables to the dir_email table. If a user is not classified 
+as a student, their email address should not end in cu.edu. If a user is a 
+student, their id should be found in the dir_acad_career table.
+
+Summary: This SQL script retrieves a distinct list of usernames, emails, and 
+their associated person type for those affiliated with the CU Boulder Campus,
+excluding certain types of affiliations and email addresses. 
+*/
